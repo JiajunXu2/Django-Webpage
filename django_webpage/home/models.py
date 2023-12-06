4543cbdddd4a418f6d4a940fbfd2776378b489a2
Jiajun Xu <jiajunxu2@gmail.com>
2023-12-05 22:04:54 -0500

    added order, order_item, customer, product, and shipping_address models

--- django_webpage/home/models.py
+++ django_webpage/home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class customer(models.Model):
+    pass
+
+class order(models.Model):
+    pass
+
+class order_item(models.Model):
+    pass
+
+class product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField()
+
+class shipping_address(models.Model):
+    address1 = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=7)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)
