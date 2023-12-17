03eb45ff3e8b1c04bc6e698a4fd284d3d594e40e
Jiajun Xu <jiajunxu2@gmail.com>
2023-12-16 20:13:25 -0500

    update customer, order, order_item, shipping model

--- django_webpage/home/models.py
+++ django_webpage/home/models.py
@@ -2,20 +2,26 @@ from django.db import models
 
 # Create your models here.
 class customer(models.Model):
-    pass
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    phone_number = models.FloatField()
 
 class order(models.Model):
-    pass
-
-class order_item(models.Model):
-    pass
+    client = models.ForeignKey(customer, on_delete=models.CASCADE)
+    date = models.FloatField()
 
 class product(models.Model):
     name = models.CharField(max_length=50)
     price = models.FloatField()
 
+class order_item(models.Model):
+    item = models.ForeignKey(product, on_delete=models.CASCADE)
+
 class shipping_address(models.Model):
+    client = models.ForeignKey(customer, on_delete=models.CASCADE)
     address1 = models.CharField(max_length=50)
-    zip_code = models.CharField(max_length=7)
+    apt_number = models.CharField(max_length = 4)
+    zip_code = models.FloatField()
     city = models.CharField(max_length=20)
     state = models.CharField(max_length=20)
